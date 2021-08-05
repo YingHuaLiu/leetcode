22b2e553e9e5678b029bf60a8df525de50bb05e6
YingHuaLiu <1042413535@qq.com>
2021-08-05 21:54:43 +0800

    2021.8.5

--- 手写系列/驼峰命名和下划线互转.js
+++ 手写系列/驼峰命名和下划线互转.js
@@ -1,13 +1,9 @@
 function humpToUnderline(name) {
   return name.replace(/([A-Z])/g, '_$1').toLowerCase();
 }
-function humpToUnderline2(name) {
-  name.map(value=>console.log(value))
-}
 function underlineToHump(name) {
   return name.replace(/_(\w)/g, (substring, args) => {
     return args.toUpperCase();
   });
 }
-[].filter()
-humpToUnderline2('aaaBbbCccDdd');
+humpToUnderline('aaaBbbCccDdd');
