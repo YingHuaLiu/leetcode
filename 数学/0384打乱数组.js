372ebb2f2685e2beeac289d5ef0d1f5afcafd2b8
YingHuaLiu <1042413535@qq.com>
2021-08-06 20:40:51 +0800

    2021.8.6

---
+++ 数学/0384打乱数组.js
@@ -0,0 +1,49 @@
+class FisherSolution {
+  constructor(nums) {
+    this.array = nums;
+    this.original = Object.assign([], nums);
+  }
+
+  reset() {
+    this.array = this.original;
+    this.original = Object.assign([], this.array);
+  }
+
+  shuffle() {
+    for (let i = 0; i < this.array.length; i++) {
+      // 注意是[i,array.length)的随机数
+      const index = Math.floor(Math.random() * (this.array.length - i) + i);
+      // 两两交换
+      [this.array[i], this.array[index]] = [this.array[index], this.array[i]];
+    }
+    return this.array;
+  }
+}
+
+let test = new FisherSolution([1, 2, 3, 4, 5]);
+console.log(test.shuffle());
+
+class Solution {
+  constructor(nums) {
+    this.array = nums;
+    this.original = Object.assign([], nums);
+  }
+
+  reset() {
+    this.array = this.original;
+    this.original = Object.assign([], this.array);
+  }
+
+  shuffle() {
+    const list = Object.assign([], this.array);
+    for (let i = 0; i < this.array.length; i++) {
+      let index = Math.floor(Math.random() * list.length);
+      this.array[i] = list[index];
+      list.splice(index, 1);
+    }
+    return this.array;
+  }
+}
+
+
+
