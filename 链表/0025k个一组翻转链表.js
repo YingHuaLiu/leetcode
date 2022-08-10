8b5ea63d0d33a25f5cc3cae8c89a7666f9f4a5e8
zdm <1042413535@qq.com>
2022-08-10 17:29:32 +0800

    2022.8.10

--- 链表/0025k个一组翻转链表.js
+++ 链表/0025k个一组翻转链表.js
@@ -1,36 +1,35 @@
 function reverseKGroup(head, k) {
   let dummy = new ListNode(-1);
   dummy.next = head;
-  let pre = dummy, end = dummy;
+  let pre = dummy, cur = dummy;
 
-  while (end.next) {
+  while (cur.next) {
     // 将end移动到k个元素的组尾
-    for (let i = 0; i < k && end; i++) {
-      end = end.next;
+    for (let i = 0; i < k && cur; i++) {
+      cur = cur.next;
     }
     // 如果不足k个元素，那么结束
-    if(!end) {
+    if (!cur) {
       break;
     }
-    let start = pre.next, next = end.next;
-    end.next = null;
+    let start = pre.next, next = cur.next;
+    cur.next = null;
     pre.next = reverse(start);
     start.next = next;
 
     pre = start;
-    end = start;
+    cur = start;
   }
   return dummy.next;
 }
 
-function reverse(start) {
-  let dummy = new ListNode(-1);
-  let cur = start;
+function reverse(head) {
+  let pre = null, cur = head;
   while (cur) {
     let next = cur.next;
-    cur.next = dummy.next;
-    dummy.next = cur;
+    cur.next = pre;
+    pre = cur;
     cur = next;
   }
-  return dummy.next;
+  return pre;
 }
--- 链表/0092反转链表 II.js
+++ 链表/0092反转链表 II.js
@@ -1,18 +1,17 @@
 var reverseBetween = function (head, left, right) {
-  let root = new ListNode(-1);
-  root.next = head;
+  let root = new ListNode(-1, head);
   let pre = root;
   // pre遍历到left节点的左边
   for (let i = 1; i < left; i++) {
     pre = pre.next;
   }
   // 其实就是每次循环将next插入到pre前面
-  let cur = pre.next, next = cur.next;
+  let cur = pre.next
   for (let i = left; i < right; i++) {
+    let next = cur.next;
     cur.next = next.next;
     next.next = pre.next;
     pre.next = next;
-    next = cur.next;
   }
   return root.next;
 };
